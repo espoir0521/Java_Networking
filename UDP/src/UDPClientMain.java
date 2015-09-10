d4b25d6fd0202ee8fc7ab48f4cc282bd641f7975
Mirim <espoir@e-mirim.hs.kr>
2015-09-10 17:23:51 +0900

    add UDP

---
+++ UDP/src/UDPClientMain.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by Mirim on 2015-09-10.
+ */
+public class UDPClientMain {
+    public static void main(String[] args) {
+        String message = "";
+        Scanner sc = new Scanner(System.in);
+
+        //input message
+        System.out.print("보낼 메시지 > ");
+        message = sc.nextLine();//한 줄 읽음.
+
+        //make packet
+
+        //make socket
+
+        //send
+
+    }
+}
---
+++ UDP/src/UDPServerMain.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Mirim on 2015-09-10.
+ */
+public class UDPServerMain {
+    public static void main(String[] args) {
+        //input message
+        //make packet
+        //receive
+        //print message
+    }
+}
